b21b543b09379de85a15628d0225ec9b983944b9
Vandra Pritesh Mehulbhai <vandrapritesh2745@gmail.com>
2021-10-12 11:10:23 +0530

    modified page

---
+++ js/scrollbar.js
@@ -0,0 +1,22 @@
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if($(window).scrollTop() > $(window).height()){
+//             $(".navigation").css({"background-color":"transparent"});   
+//         }
+//         else{
+//             $(".navigation").css({"background-color":"white"});
+//         }
+
+//     })
+// })
+$(document).ready(function(){
+    $(window).scroll(function(){
+      if(this.scrollY > 20){
+        $(".navbar").addClass("sticky");
+        $(".goTop").fadeIn();
+      }
+      else{
+        $(".navbar").removeClass("sticky");
+        $(".goTop").fadeOut();
+      }
+    });
